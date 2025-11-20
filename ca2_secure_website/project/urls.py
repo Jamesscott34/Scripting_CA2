d608408999451780d59ebf82b42d293498d2c2a8
james scott <jamesdeanscott19@gmail.com>
2025-11-20 18:30:56 +0000

    Add explicit static file serving for CA2 development

--- ca2_secure_website/project/urls.py
+++ ca2_secure_website/project/urls.py
@@ -1,5 +1,7 @@
 """Root URL configuration for the CA2 banking project."""
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -9,4 +11,6 @@ urlpatterns = [
     path("", include("app.urls")),
 ]
 
+# Serve static files in development (and for CA2 demos).
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
